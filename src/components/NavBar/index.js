060509e048cac376f27dbe0bbfbc479ddcc18f43
mansip2002 <mansii.patel@mail.utoronto.ca>
2021-05-30 23:29:21 -0400

    initial

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {FaBars} from 'react-icons/fa';
+import {Nav, NavbarContainer, Navlogo, MobileIcon, NavMenu, NavItem, NavLinks} from './navElements';
+
+const NavBar = () => {
+    return (
+        <React.Fragment> 
+            <Nav>
+                <NavbarContainer>
+                    <Navlogo to='/'>home</Navlogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to='about'>About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='discover'>Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='services'>Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to='contact'>Contact</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to="/signin">Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </React.Fragment>
+    );
+};
+
+export default NavBar;
